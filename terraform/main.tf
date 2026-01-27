017e03e4b3100eae8e795f9ad1680ed3ba649191
Sam North <samuelnorth47@gmail.com>
2026-01-26 18:46:32 -0600

    init commit

---
+++ terraform/main.tf
@@ -0,0 +1,102 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.2-rc07"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://192.168.20.11:8006/api2/json"
+  pm_api_token_id = "root@pam!terraform-token"
+  pm_api_token_secret = "62abc389-8cdb-48dc-b488-f0da3892a544"
+  pm_tls_insecure = "true"
+}
+
+resource "proxmox_lxc" "gh-actions" {
+  target_node  = "mercury"
+  hostname     = "gh-actions"
+  ostemplate   = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
+  unprivileged = true
+
+  cores = 1
+  memory = 2048
+  start = true
+
+  ssh_public_keys = <<-EOT
+    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCv7OVeUsXnds8CNYb6tY90FXy7tA1Q/3HUhVl7BbnL6l1jA7/sHBL56sooXnKq5jqfXWxcCmnnPhUqAUtOJIffnduQZA0V3VFX78ZZui3BhYBi1YjEU3p4svIfAEl7bIIQn0AOdG1D3AyBE7n1n988JmDkRsS4wgfH+1QgGjezf8ySPqQhMza7uvzoi+AFo8HOEAbKyFZDIAGOMrogaSDFQ5/RBXdVWQnthN2mEIasKOnZtS0ppVjn1SiQCVovaVTgOGWq3oLpPqbOaatYv1BhNyxLf6TO+FN5JNW581UPbJTbpTc0TwecfnZQE51fxNq71Nj7DWF+hC/StQlfm9PmiR3krRsjZrch5WVCS7uB1CNvqaYgWP9ME8aQa589ieOtCXjQ3f1pWYQXuDHjzaN3w/d+4J9fSdNq9o3Bys6kBTfaSGE2Lg0vnwyopDtly+dBnp9BjDANNb6K8vcDNim+a7NSwiI79pqs4BLDSf5O2aSy0gHiw8dTEkmCwxlJg6bQAccsmZV2NsDRIpr3PT7RR3N/GbhooOGFSK/iBGwkk3218jvDt3kVlpPW8fd1VdaKtqX94RM4f6PHQssPQDDv89i4/RIHeMPNUP8V0R1SaoKUIYNg8g52rfK1NKp1TlZaLTAe+mlCbWXNeh56xMIE3ENPJ83ndyhPtp63J1NfAw== samuelnorth47@fedora
+    EOT
+
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "local"
+    size    = "8G"
+  }
+
+  // Storage Backed Mount Point
+#   mountpoint {
+#     key     = "0"
+#     slot    = 0
+#     storage = "local-lvm"
+#     mp      = "/mnt/container/storage-backed-mount-point"
+#     size    = "12G"
+#   }
+
+#   // Bind Mount Point
+#   mountpoint {
+#     key     = "1"
+#     slot    = 1
+#     storage = "/srv/host/bind-mount-point"
+#     // Without 'volume' defined, Proxmox will try to create a volume with
+#     // the value of 'storage' + : + 'size' (without the trailing G) - e.g.
+#     // "/srv/host/bind-mount-point:256".
+#     // This behaviour looks to be caused by a bug in the provider.
+#     volume  = "/srv/host/bind-mount-point"
+#     mp      = "/mnt/container/bind-mount-point"
+#     size    = "256G"
+#   }
+
+#   // Device Mount Point
+#   mountpoint {
+#     key     = "2"
+#     slot    = 2
+#     storage = "/dev/sdg"
+#     volume  = "/dev/sdg"
+#     mp      = "/mnt/container/device-mount-point"
+#     size    = "32G"
+#   }
+  nameserver = "192.168.20.1"
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    gw     = "192.168.20.1"
+    ip     = "192.168.20.55/24"
+  }
+}
+
+resource "null_resource" "install_ssh" {
+  depends_on = [proxmox_lxc.gh-actions]
+    provisioner "local-exec" {
+  command = <<EOF
+    ssh root@192.168.20.11 "pct exec ${proxmox_lxc.gh-actions.vmid} -- bash -c '
+    set -e
+    apt update
+    apt install -y openssh-server
+    systemctl enable --now ssh
+    '"
+    EOF
+    }
+}
+
+
+resource "null_resource" "run_ansible" {
+  depends_on = [null_resource.install_ssh]
+
+  provisioner "local-exec" {
+    command = <<EOF
+    cd ../ansible && ansible-playbook site.yaml 
+    EOF
+  }
+}
